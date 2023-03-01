f09a4bf1b31c8ed3662bdbe7699579b8cfe13ff3
knvolap <knvolap@gmail.com>
2023-03-01 10:50:59 +0700

    Initial commit

---
+++ FindAllPossibleSubstring/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FindAllPossibleSubstring
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "abcd";
+            PrintAllSubstrings(input);
+        }
+
+        public static void PrintAllSubstrings(string str)
+        {
+            // Loop through each character in the string
+            for (int i = 0; i < str.Length; i++)
+            {
+                // Generate all substrings starting from this character
+                for (int j = i + 1; j <= str.Length; j++)
+                {
+                    Console.WriteLine(str.Substring(i, j - i));
+                }
+            }
+        }
+
+
+    }
+}
---
+++ InterviewCodingQuestion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewCodingQuestion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "tmafeshermarch";
+            string output = RemoveDuplicateCharacters(input);
+            Console.WriteLine(output);
+           
+        }
+
+        public static string RemoveDuplicateCharacters(string str)
+        {
+            // Create a new list to store unique characters
+            List<char> uniqueChars = new List<char>();
+
+            // Loop through each character in the string
+            foreach (char c in str)
+            {
+                // If the character is not already in the list, add it
+                if (!uniqueChars.Contains(c))
+                {
+                    uniqueChars.Add(c);
+                }
+            }
+
+            // Convert the list to a string and return it
+            return new string(uniqueChars.ToArray());
+            //This line converts the uniqueChars list to a character array using the ToArray method,
+            // and then creates a new string from the character array using the string constructor that
+            // takes a character array as input.Finally, the resulting string with duplicates removed
+            // is returned from the method.
+        }
+
+        //Overall, this method works by looping through each character in the input string,
+        //adding it to a list of unique characters if it is not already present,
+        //and then converting the list to a new string with duplicates removed.
+    }
+}
+
